ff89eb535df61fdf905298d05a82c6f86975ff8f
Noa  Amram <noaamram9421@gmail.com>
2025-12-22 16:19:15 +0200

    Remove logging setup from client main
    Removed logging setup from the main function.

--- tictactoe_online/client/main.py
+++ tictactoe_online/client/main.py
@@ -4,18 +4,14 @@ Client main entry point.
 """
 
 import sys
-import logging
+
 from PyQt5.QtWidgets import QApplication
 from .main_window import MainWindow
 
 
 def main():
     """Main function to run the client application."""
-    # Set up logging
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
+ 
     
     # Create Qt application
     app = QApplication(sys.argv)
